f18f997e2d87c9089a11ea2d0b9270b2731ec209
jonathan-robertson <jrob356@icloud.com>
2024-12-27 05:43:32 -0600

    add mod and log classes

---
+++ src/ModApi.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using OffWorldFix.Utilities;
+using System;
+using System.Reflection;
+
+namespace OffWorldFix
+{
+    internal class ModApi : IModApi
+    {
+        private const string ModMaintainer = "kanaverum";
+        private const string SupportLink = "https://discord.gg/hYa2sNHXya";
+
+        private static readonly ModLog<ModApi> _log = new ModLog<ModApi>();
+
+        public static bool DebugMode { get; set; } = false;
+
+        public void InitMod(Mod _modInstance)
+        {
+            try
+            {
+                new Harmony(GetType().ToString()).PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception e)
+            {
+                _log.Error($"Failed to start up Off World Fix mod; take a look at logs for guidance but feel free to also reach out to the mod maintainer {ModMaintainer} via {SupportLink}", e);
+            }
+        }
+    }
+}
